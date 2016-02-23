737d0ba77fdae92e8b4a097f962889434f19c3af
Steve Gerhard <steven.gerhard@cigna.com>
2016-02-23 13:06:17 -0500

    bug fix

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ io.on('connection', function (socket) {
 });
 
 server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
